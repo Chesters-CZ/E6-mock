a07c5343db3437cfa6fc6508e6483af1b8ccaebb
Chesters-CZ <62833819+Chesters-CZ@users.noreply.github.com>
2024-04-18 14:53:39 +0200

    step13 and 14 done

---
+++ pičon/step13.py
@@ -0,0 +1,110 @@
+import csv
+import sys
+import time
+import winsound
+
+def load_csv(path, header_return, data_return, has_header):
+    print("Loading " + path)
+    with (open("D:\\velký dbs fily\\" + path, 'r', encoding="utf-8") as file_in):
+        file_reader = csv.reader(file_in)
+
+        print("0.0%", end="")
+        lines = 0
+        for line in file_reader:
+            lines += 1
+        file_in.seek(0)
+        lines = (lines / 1000).__floor__()
+        if lines == 0:
+            lines = 1
+
+        firstline = True
+        for i, row in enumerate(file_reader):
+            if i % lines == 0:
+                print("\r" + (i / lines / 10).__str__() + "%", end="")
+
+            if firstline:
+                if has_header:
+                    header_return.clear()
+                    header_return.append(row)
+                else:
+                    data_return.append(row)
+                firstline = False
+                continue
+            data_return.append(row)
+    print("\nDone!\n")
+
+
+def dump_database(dbs: list, header: list, filename: str):
+    with open("D:\\velký dbs fily\\" + filename, "w", encoding="utf-8", newline="") as file_out:
+        writer = csv.writer(file_out)
+        writer.writerow(header)
+
+        arrlen = (dbs.__len__() / 1000).__floor__()
+        for i, row in enumerate(dbs):
+            if i % arrlen == 0:
+                print("\r" + (i / arrlen / 10).__str__() + "%", end="")
+            writer.writerow(row)
+    print("\nDone\n")
+
+
+def dump_single_column_database(dbs: list, header: list, filename: str):
+    with open("D:\\velký dbs fily\\" + filename, "w", encoding="utf-8", newline="") as file_out:
+        writer = csv.writer(file_out)
+        writer.writerow(header)
+
+        arrlen = (dbs.__len__() / 1000).__floor__()
+        for i, row in enumerate(dbs):
+            if i % arrlen == 0:
+                print("\r" + (i / arrlen / 10).__str__() + "%", end="")
+            writer.writerow([row])
+    print("\nDone\n")
+
+
+
+maxInt = sys.maxsize
+
+print("Increasing csv field size limit...")
+while True:
+    try:
+        csv.field_size_limit(maxInt)
+        break
+    except OverflowError:
+        maxInt = int(maxInt / 10)
+        print(".", end="")
+
+print("\nDone!\n")
+
+posts = []
+discard = []
+
+load_csv("step7\\posts.csv", discard, posts, True)
+
+post_ids = [post[0] for post in posts]
+
+favorites = []
+
+load_csv("step11\\favorites.csv", discard, favorites, False)
+
+favorites_final = [fav for fav in favorites if fav[1] in post_ids]
+
+comments = []
+
+load_csv("step12\\comment_text_post.csv", discard, comments, False)
+
+comments_final = [comment for comment in comments if comment[3] in post_ids]
+
+dump_database(favorites_final, [], "step13\\favorites.csv")
+
+dump_database(comments_final, [], "step13\\comments.csv")
+
+
+
+print("\nDone!\n")
+
+print("change the world;")
+time.sleep(2)
+print("my final message.")
+time.sleep(3)
+print("goodbye")
+time.sleep(3)
+winsound.PlaySound("./o95.wav", winsound.SND_FILENAME)
---
+++ pičon/step14.py
@@ -0,0 +1,152 @@
+import csv
+import re
+import sys
+import time
+
+import requests
+import winsound
+from bs4 import BeautifulSoup
+
+
+def load_csv(path, header_return, data_return, has_header):
+    print("Loading " + path)
+    with (open("D:\\velký dbs fily\\" + path, 'r', encoding="utf-8") as file_in):
+        file_reader = csv.reader(file_in)
+
+        print("0.0%", end="")
+        lines = 0
+        for line in file_reader:
+            lines += 1
+        file_in.seek(0)
+        lines = (lines / 1000).__floor__()
+        if lines == 0:
+            lines = 1
+
+        firstline = True
+        for i, row in enumerate(file_reader):
+            if i % lines == 0:
+                print("\r" + (i / lines / 10).__str__() + "%", end="")
+
+            if firstline:
+                if has_header:
+                    header_return.clear()
+                    header_return.append(row)
+                else:
+                    data_return.append(row)
+                firstline = False
+                continue
+            data_return.append(row)
+    print("\nDone!\n")
+
+
+def dump_database(dbs: list, header: list, filename: str):
+    with open("D:\\velký dbs fily\\" + filename, "w", encoding="utf-8", newline="") as file_out:
+        writer = csv.writer(file_out)
+        writer.writerow(header)
+
+        arrlen = (dbs.__len__() / 1000).__floor__()
+        for i, row in enumerate(dbs):
+            if i % arrlen == 0:
+                print("\r" + (i / arrlen / 10).__str__() + "%", end="")
+            writer.writerow(row)
+    print("\nDone\n")
+
+
+def dump_single_column_database(dbs: list, header: list, filename: str):
+    with open("D:\\velký dbs fily\\" + filename, "w", encoding="utf-8", newline="") as file_out:
+        writer = csv.writer(file_out)
+        writer.writerow(header)
+
+        arrlen = (dbs.__len__() / 1000).__floor__()
+        for i, row in enumerate(dbs):
+            if i % arrlen == 0:
+                print("\r" + (i / arrlen / 10).__str__() + "%", end="")
+            writer.writerow([row])
+    print("\nDone\n")
+
+
+def get_user_blips(user_id: int):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"}
+    response = requests.get('https://e621.net/blips?group_by=comment&search%5Bcreator_id%5D=' + str(user_id),
+                            headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    blips = soup.find_all("div", class_="response-list")
+    blips_out = []
+    for blip in blips:
+        try:
+            blips_out.append([blip.findNext("div", class_="post-time").findChildren()[0]["href"][7:],
+                              user_id,
+                              blip.findNext("div", class_="styled-dtext").text,
+                              ""])
+        except AttributeError:
+            pass
+    return blips_out
+
+
+maxInt = sys.maxsize
+
+print("Increasing csv field size limit...")
+while True:
+    try:
+        csv.field_size_limit(maxInt)
+        break
+    except OverflowError:
+        maxInt = int(maxInt / 10)
+        print(".", end="")
+
+print("\nDone!\n")
+
+print("\nWARNING: SCRIPT APPENDS TO step14\\blip_text_post.csv AND WILL NOT REPLACE EXISTING DATA\n")
+
+discard = []
+already_scraped = []
+
+load_csv("step14\\blip_text_post.csv", discard, already_scraped, False)
+
+already_scraped_ids = [blip[1] for blip in already_scraped]
+del already_scraped
+
+raw_user_ids = []
+
+load_csv("step9\\user_ids_unique.csv", discard, raw_user_ids, True)
+
+# todo: check if this works
+user_ids_to_be_scraped = [uid for uid in raw_user_ids if uid[0] not in already_scraped_ids]
+del raw_user_ids
+del already_scraped_ids
+
+with open("D:\\velký dbs fily\\step14\\blip_text_post.csv", mode="a", encoding="utf-8", newline="") as blips_out:
+    blips_writer = csv.writer(blips_out)
+
+    linecount = (user_ids_to_be_scraped.__len__() / 1000).__floor__()
+    for i, user in enumerate(user_ids_to_be_scraped):
+        start_time = time.time()
+        print(
+            "\r Scraping user " + user[
+                0] + " (" + i.__str__() + "/" + user_ids_to_be_scraped.__len__().__str__() + " " + (
+                    i / linecount / 10).__str__() + "%)", end="")
+
+        try:
+            comments = get_user_blips(user[0])
+        except Exception as e:
+            print("ERROR WHEN SCRAPING COMMENTS OF USER " + user[0])
+            print(e.__str__())
+            break
+
+        print(" " + comments.__len__().__str__() + " found in " + (time.time() - start_time).__str__() + "s",
+              end="")
+
+        for comment in comments:
+            blips_writer.writerow(comment)
+        time.sleep(0.5)
+
+print("\nDone!\n")
+
+print("change the world;")
+time.sleep(2)
+print("my final message.")
+time.sleep(3)
+print("goodbye")
+time.sleep(3)
+winsound.PlaySound("./o95.wav", winsound.SND_FILENAME)
